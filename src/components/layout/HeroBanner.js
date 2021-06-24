9249da5bfe1a633a400ff6bcae4b4bf45f51ee00
Alexandra Eline Pettersen <57686601+frontalexandraep@users.noreply.github.com>
2021-06-25 01:36:38 +0200

    Styling

---
+++ src/components/layout/HeroBanner.js
@@ -0,0 +1,16 @@
+export default function HeroBanner() {
+  return (
+    <div className="herobanner">
+      <div className="herobanner__content">
+        <div className="herobanner__content__title">
+          <h1>15 React Projects</h1>
+          <h2>
+            Visit my website <a href="https://frontalex.com/">FrontAlex</a> for
+            more
+          </h2>
+        </div>
+        <img src="../images/react.svg" alt="React logo" />
+      </div>
+    </div>
+  );
+}
--- src/components/layout/Projects.js
+++ src/components/layout/Projects.js
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
+import HeroBanner from "./HeroBanner";
 
 export default function Projects() {
   return (
-    <div>
-      <Link to="/birthday-reminder" target="_blank" rel="noopener noreferrer">
-        Birthday Reminder
-      </Link>
-      <Link to="/tours" target="_blank" rel="noopener noreferrer">
-        Tours
-      </Link>
-      <Link to="/reviews" target="_blank" rel="noopener noreferrer">
-        Reviews
-      </Link>
-      <Link to="/faq" target="_blank" rel="noopener noreferrer">
-        FAQ Accordion
-      </Link>
-      <Link to="/menu" target="_blank" rel="noopener noreferrer">
-        Menu
-      </Link>
+    <div className="projects">
+      <HeroBanner />
+      <div className="projects__links">
+        <Link to="/birthday-reminder" target="_blank" rel="noopener noreferrer">
+          Birthday Reminder
+        </Link>
+        <Link to="/tours" target="_blank" rel="noopener noreferrer">
+          Tours
+        </Link>
+        <Link to="/reviews" target="_blank" rel="noopener noreferrer">
+          Reviews
+        </Link>
+        <Link to="/faq" target="_blank" rel="noopener noreferrer">
+          FAQ Accordion
+        </Link>
+        <Link to="/menu" target="_blank" rel="noopener noreferrer">
+          Menu
+        </Link>
+      </div>
     </div>
   );
 }
